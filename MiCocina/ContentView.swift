540f643852bafc39110f3c1ba6d685c772d7c948
Carlos Cardoso <charly-cardoso@outlook.com>
2026-04-13 18:59:33 -0600

    [UPDATE] content view with menu added

--- MiCocina/ContentView.swift
+++ MiCocina/ContentView.swift
@@ -17,12 +17,58 @@ import SwiftData
 ///   context for any future data operations.
 ///
 /// - TODO: Implement recipe display, filtering, and interaction components
+
+enum NavigationViews {
+    case Home
+    case MyPantry
+    case Planner
+    case ShoppingList
+}
 struct ContentView: View {
     /// The SwiftData model context for database operations
     @Environment(\.modelContext) private var modelContext
+    @State private var navigation: NavigationViews = .Home
     
     var body: some View {
-        Text("Hello, world!")
+        TabView(selection: $navigation) {
+            Tab(String(localized: "homeContent.navigationTitle"), systemImage: "house.fill", value: .Home) {
+                NavigationStack {
+                    Navigate(to: .Home)
+                }
+            }
+            
+            Tab(String(localized: "myPantry.navigationTitle"), systemImage: "refrigerator.fill", value: .MyPantry) {
+                NavigationStack {
+                    Navigate(to: .MyPantry)
+                }
+            }
+            
+            Tab(String(localized: "planner.title"), systemImage: "calendar", value: .Planner) {
+                NavigationStack {
+                    Navigate(to: .Planner)
+                }
+            }
+            
+            Tab(String(localized: "shoppingList.title"), systemImage: "cart.fill", value: .ShoppingList) {
+                NavigationStack {
+                    Navigate(to: .ShoppingList)
+                }
+            }
+        }
+    }
+
+    @ViewBuilder
+    private func Navigate(to: NavigationViews) -> some View {
+        switch to {
+        case .Home:
+            HomeContent(viewModel: .init(context: modelContext))
+        case .MyPantry:
+            MyPantryView(viewModel: .init(context: modelContext))
+        case .Planner:
+            PlannerView(viewModel: .init(context: modelContext))
+        case .ShoppingList:
+            ShoppingListView()
+        }
     }
 }
 
